be402b45f15966d9cc1dae17988db9712659933e
Diego <diegopena2022@gmail.com>
2022-11-26 21:24:07 -0800

    setup full-stack structure

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const { ApolloServer } = require('apollo-server-express');
 
+const path = require('path');
+
 const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
@@ -25,6 +27,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
 
   server.applyMiddleware({ app });
+
+  // Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {

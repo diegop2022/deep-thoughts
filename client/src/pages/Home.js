e0f6933a6f54cb5d9d62bcbf29b2207120e4b8c9
Diego <diegopena2022@gmail.com>
2022-12-01 15:39:32 -0800

    added thought functionallity

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,7 @@ import ThoughtList from '../components/ThoughtList';
 import { QUERY_ME_BASIC } from '../utils/queries';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Home = () => {
   // use useQuery hook to make query request
@@ -19,6 +20,11 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
